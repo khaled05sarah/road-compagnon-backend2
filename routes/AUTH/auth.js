6f53fd47f086631f4f56fd3db70827f3ac96d45f
khaled05sarah <ns_khaled@esi.dz>
2025-05-10 19:37:15 +0100

    initial commit

--- routes/AUTH/auth.js
+++ routes/AUTH/auth.js
@@ -21,7 +21,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS1,
+        pass: process.env.EMAIL_PASS,
     },
 });
 
